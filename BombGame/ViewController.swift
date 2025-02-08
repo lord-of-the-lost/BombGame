e280750f3c5c63ff8e1ba3abb17c7e784783e2ca
lord-of-the-lost <ignatov.99@yandex.ru>
2025-02-08 21:35:23 +0300

    Initial Commit

---
+++ BombGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BombGame
+//
+//  Created by Николай Игнатов on 08.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
